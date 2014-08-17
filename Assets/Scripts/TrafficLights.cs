1e57407f70e7a322c209373abf0c618e051ea2f7
Daniil Basmanov <daniil.basmanov@cloudmach.com>
2014-08-17 21:25:27 +0400

    Working traffic lights

--- Assets/Scripts/TrafficLights.cs
+++ Assets/Scripts/TrafficLights.cs
@@ -3,26 +3,45 @@ using UnityEngine;
 
 public class TrafficLights : MonoBehaviour
 {
+    public GameObject red;
+    public GameObject yellow;
+    public GameObject green;
+
     public void StartCountdown(int count, Action onStep, Action onFinished)
     {
-        Pulse(() =>
+        if (count == 1)
         {
-            count--;
-            if (count > 0)
-            {
-                onStep();
-                StartCountdown(count, onStep, onFinished);
-            }
-            else
-            {
-                onFinished();
-            }
-        });
+            LeanTween.scale(yellow, Vector3.zero, 0.5f);
+            Pulse(green, () => CheckCount(count, onStep, onFinished));
+        }
+        else if (count == 2)
+        {
+            LeanTween.scale(red, Vector3.zero, 0.5f);
+            Pulse(yellow, () => CheckCount(count, onStep, onFinished));
+        }
+        else
+        {
+            Pulse(red, () => CheckCount(count, onStep, onFinished));
+        }
     }
 
-    private void Pulse(Action onFinished)
+    private void CheckCount(int count, Action onStep, Action onFinished)
     {
-        LeanTween.scale(gameObject, Vector3.zero, 0.5f).onComplete =
-            () => { LeanTween.scale(gameObject, Vector3.one, 0.5f).onComplete = onFinished; };
+        count--;
+        if (count > 0)
+        {
+            onStep();
+            StartCountdown(count, onStep, onFinished);
+        }
+        else
+        {
+            onFinished();
+        }
+    }
+
+    private void Pulse(GameObject go, Action onFinished)
+    {
+        LeanTween.scale(go, Vector3.zero, 0.5f).onComplete =
+            () => { LeanTween.scale(go, Vector3.one, 0.5f).onComplete = onFinished; };
     }
 }
